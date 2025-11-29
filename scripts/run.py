b61008b6d3303230ca2c1fdd997cf2e5884dfcb0
Rohan <bc@gmail.com>
2025-11-29 11:31:06 +0530

    added python script

---
+++ scripts/run.py
@@ -0,0 +1,13 @@
+import socket
+import os
+
+print(os.name)
+print(socket.gethostname())
+
+path = "~/config/message.txt"
+
+with open (path, "r") as file:
+    content = file.read()
+    print("\nThese are the contents inside config/\n" +content)
+
+print("\n Printing secret text from github secrets " + ${{secrets.SECRET_MESSAGE}})
